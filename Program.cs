e6be21b7ab6d69e300f0b261f52e2096003f0ab6
nicol <nicol@BLUE_LAPTOP>
2024-03-10 12:55:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace immutableID
+{
+    class Program
+    {
+        public record Student(string FirstName, string LastName)
+        {
+
+            // auto-implemented properties
+            public int Id { get; init; }
+            //public string FirstName { get; set; }
+            //public string LastName { get; set; }
+        }
+        public static void Main()
+        {
+
+            // default constructor to enter ID
+            Student student1 = new("Scooby", "Doo")
+            {
+                Id = 1
+            };
+            Console.WriteLine(student1);
+            
+            // parameterized constructor with blanks for names
+            Student student = new("", "")
+            {
+                Id = 0
+            };
+            //Console.WriteLine(student);
+            //don't need to print the blank student
+            
+            //parameterized constructor with student info- uses blank Id info
+            Student student3 = new("Scrappy", "Doo");
+            Console.WriteLine(student3);
+
+        }
+    }
+
+}
+
+        
+    
